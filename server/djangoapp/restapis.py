01606571dfdf07f4cd1037e8199d68b95f693ac4
Munmi Baruah <munmi@positra.com>
2024-06-28 17:37:05 +0530

    correction9

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -29,6 +29,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print(f"Network exception occurred: {e}")
 
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -44,8 +45,8 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
