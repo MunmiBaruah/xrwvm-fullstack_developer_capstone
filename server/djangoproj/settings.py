3b31c1e43bee4f6bddfa169033ed8f74a4a5552f
Munmi Baruah <munmi@positra.com>
2024-06-28 22:05:19 +0530

    correction23

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://baruahmunmi5-8000.theiadockernext-1-labs'+
+    'https://baruahmunmi5-8000.theiadockernext-1-labs' +
     '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://baruahmunmi5-8000.theiadockernext-1-labs'+
+    'https://baruahmunmi5-8000.theiadockernext-1-labs' +
     '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
@@ -101,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation' +
+        '.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -150,4 +151,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
